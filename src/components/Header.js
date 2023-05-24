adf6d4d2c0cbca37f6dce7cb7533a02acb62c361
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-05-24 09:07:44 +0530

    small UI changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,22 +9,22 @@ const Header = () => {
         <img src={Rectangle} className="w-full" alt="banner" />
         <div className="absolute bottom-8 left-96 top-4 right-0 mx-4">
           <div className="rounded-lg p-4 flex items-center justify-between">
-            <div className="bg-white rounded-lg p-4 ml-4">
+            <div className="bg-white rounded-lg p-4">
               <div className="flex flex-row justify-center items-center">
                 <label htmlFor="" className="text-black mr-4">
                   Last 30 days:
                 </label>
-                <div className="bg-green-400 mr-2 text-white rounded-lg px-4 py-2">
+                <div className="bg-green-500 mr-1 text-white rounded-lg px-3 py-2">
                   <span className="uppercase">Paid</span>
                   <br />
                   €24,000
                 </div>
-                <div className="bg-slate-400 mr-2 text-white px-4 py-2 rounded-lg">
+                <div className="bg-slate-500 mr-1 text-white w-28 text-start px-3 py-2 rounded-lg">
                   <span className="uppercase">Pending</span>
                   <br />
                   €41,000
                 </div>
-                <div className="bg-red-400 px-4 text-white py-2 rounded-lg">
+                <div className="bg-red-500 w-40 px-3 text-start text-white py-2 rounded-lg">
                   <span className="uppercase">Overdue</span>
                   <br />
                   €65,600
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,35 +12,35 @@ const Sidebar = () => {
   return (
     <section>
       <div className="w-full absolute -mt-44 h-full">
-        <ul className="py-4 px-10 fixed w-full md:w-[270px] h-auto bg-white shadow-lg mt-10 mr-20 mx-8 rounded-xl">
-          <div className="bg-white rounded-lg flex p-4">
+        <ul className="py-4 px-4 fixed w-full md:w-[270px] h-auto bg-white shadow-lg mt-10 mr-20 mx-8 rounded-xl">
+          <div className="bg-white rounded-lg flex pt-4">
             <div className="bg-slate-200 rounded-full w-7 h-7 mr-7 flex justify-center items-center">
               <img src={LeftArrow} alt="" />
             </div>
 
             <img src={Logo} className="w-24" alt="logo" />
           </div>
-          <li className="flex items-center font-bold hover:bg-violet-100 rounded-md px-6 mt-20 py-6 text-black my-4">
+          <li className="flex items-center font-bold hover:bg-violet-100 rounded-md px-6 mt-10 py-4 text-black my-4">
             <img src={HomeIcon} className="mr-4" alt="Home" />
             Home
           </li>
-          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-6 text-black my-4">
+          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-4 text-black my-4">
             <img src={Invoices} className="mr-4" alt="Invoices" />
             Invoices
           </li>
-          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-6 text-black my-4">
+          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-4 text-black my-4">
             <img src={Customers} className="mr-4" alt="Customers" />
             Customers
           </li>
-          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-6 text-black my-4">
+          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-4 text-black my-4">
             <img src={Products} className="mr-4" alt="Products" />
             Products
           </li>
-          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-6 text-black my-4">
+          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-4 text-black my-4">
             <img src={Recurring} className="mr-4" alt="Recurring" />
             Recurring
           </li>
-          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-6 text-black my-4">
+          <li className="flex items-center px-6 font-bold hover:bg-violet-100 rounded-md py-4 text-black my-4">
             <img src={Discount} className="mr-4" alt="Discount" />
             Discount
           </li>
